2bd34a4c2d3082131433b09e944c6e30de5b7132
KAA3096 <pinyasova10@mail.ru>
2025-05-23 00:31:17 +0300

    Update Титульный_лист.py

--- Титульный_лист.py
+++ Титульный_лист.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import qrcode
 
 st.set_page_config(page_title="👋", layout="wide")
 
@@ -76,9 +75,6 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
-
-qr = qrcode.make("https://github.com/KAA3096/Plate")
-qr.save("qr_github.png")
 
 with col1:
     st.subheader("🔗 GitHub-репозиторий")
